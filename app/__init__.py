aacf6be8436b5868746bf054fc3b353a63c54b99
Billyzxc <35462953+Billyzxc@users.noreply.github.com>
2025-05-18 19:22:16 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from .models import db
 from .routes import main
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = "your_secret_key"  # 建議用環境變數取代
+    app.config['SECRET_KEY']='quote_secret'
+    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///quotes.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
     app.register_blueprint(main)
     return app
