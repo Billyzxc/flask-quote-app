0d266fd5a922bfa5611f84c5fcc066023ea0ac1d
Billyzxc <35462953+Billyzxc@users.noreply.github.com>
2025-05-19 16:22:15 +0800

    Delete app directory

--- app/models/user.py
+++
@@ -1,4 +0,0 @@
-from app import db
-class User(db.Model):
- id=db.Column(db.Integer,primary_key=True)
- email=db.Column(db.String(100))
--- app/routes/main_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, redirect, url_for
-
-bp = Blueprint('main', __name__)
-
-@bp.route('/')
-def index():
-    return redirect(url_for('quote.get_all_quotes'))  # 指向 quote blueprint 中的 /quotes
-
