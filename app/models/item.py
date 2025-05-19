679dbb66ae18c2f167514b9817f4804918192f70
Billyzxc <35462953+Billyzxc@users.noreply.github.com>
2025-05-19 16:55:22 +0800

    Delete app directory

--- app/models/item.py
+++
@@ -1,5 +0,0 @@
-from app import db
-class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    code = db.Column(db.String(20), unique=True)
-    name = db.Column(db.String(100))
--- app/routes/__init__.py
+++
@@ -1,3 +0,0 @@
-def register_blueprints(app):
-    from .quote_routes import quote_bp
-    app.register_blueprint(quote_bp, url_prefix="/quotes")
